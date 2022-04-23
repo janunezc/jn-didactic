32fe981aaa19c204ecea7107d0d6d6316103ee1b
jn <jose.a.nunez@gmail.com>
2022-04-22 23:32:18 -0600

    Fix pseudo code/explanatioins

--- invert-linked-list.js
+++ invert-linked-list.js
@@ -31,13 +31,13 @@
     i,m,n
     1,3,6
     1>2>3>4>5>6>7>null m:3, n:6
-    i=1:   1>2                i<m-1
-    i=2*:  1>2?               i==m-1    2? = invertHead
-    i=3*:  1>2?3?             i==m      3? = invertTail
-    i=4*:  1>2?4>3?           i>m && i<n
-    i=5*:  1>2?5>4>3?         i>m && i<n
-    i=6*:  1>2>6>5>4>3?       i==n > resolve invertHead.next
-    i=7!:  1>2>6>5>4>3>7>null i>n
+    i=1:   1>2
+    i=2*:  1>2?               i==m-1      invertEntry
+    i=3*:  1>2?3?             i==m        headToInvert
+    i=4*:  1>2?4>3?
+    i=5*:  1>2?5>4>3?
+    i=6*:  1>2>6>5>4>3?       i==n        tailToInvert
+    i=7!:  1>2>6>5>4>3>7>null i>n         invertExit
 
     !RESULT: 1>2>6>5>4>3>7>null
 
@@ -53,39 +53,39 @@
     let headToInvert = null; //begining of the list that must be inverted
     let tailToInvert = null; //end of the list that must be inverted
 
-    while (currentNode) {
-      if (currentPos === m - 1) {
-        invertEntry = currentNode;
+    while (currentNode) { //            1>2     2>3     3>4     4>5     5>6     6>7       7>null
+      if (currentPos === m - 1) { //    1vs2    2vs2    3vs2    4vs2    5vs2    6vs2      7vs2
+        invertEntry = currentNode; //            2>3     2>3     2>3     2>3     2>3      2>3
       }
 
-      if (currentPos === m) {
-        headToInvert = currentNode;
+      if (currentPos === m) { //         1vs3    2vs3    3vs3    4vs3    5vs3    6vs3     7vs3
+        headToInvert = currentNode; //                   3>4     3>4     3>4     3>4      3>4
       }
 
-      if (currentPos === n) {
-        tailToInvert = currentNode;
+      if (currentPos === n) { //         1vs6    2vs6    3vs6    4vs6    5vs6    6vs6     7vs6
+        tailToInvert = currentNode; //                                           6>7      6>7
       }
 
-      if (currentPos > n) {
-        invertExit = currentNode;
-        break;
+      if (currentPos > n) { //           1vs6    2vs6    3vs6    4vs6    5vs6    6vs6     7vs6*
+        invertExit = currentNode; //                                                      7>null
+        break; //                                                                         BREAK!
       }
 
-      currentNode = currentNode.next;
-      currentPos++;
+      currentNode = currentNode.next; // 2>3     3>4     4>5     5>6     6>7      7>null
+      currentPos++; //                   2       3       4       5       6        7
     }
 
-    tailToInvert.next = null
+    tailToInvert.next = null; //                                                          6>null
 
-    let invertedPart = invertLinkedList(headToInvert);
+    let invertedPart = invertLinkedList(headToInvert); //                                 (3>4>5>6>null) => (6>5>4>3>null)
 
-    invertExit ? headToInvert.next = invertExit : null;
+    invertExit ? headToInvert.next = invertExit : null; //                                3>...null ==> 3>...7>null
 
-    if(invertEntry){
-      invertEntry.next = invertedPart;
-      return head;
+    if (invertEntry) { //                                                                 2>3
+      invertEntry.next = invertedPart; //                                                 2>6>5>4>3>null
+      return head; //                                                                     1>2>6>5>4>3>7>null
     } else {
-      return invertedPart;
+      return invertedPart; //
     }
   }
 
